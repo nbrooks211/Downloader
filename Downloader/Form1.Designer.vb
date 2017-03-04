e9da83648131278812447ffaef981599e0b598c5
nbrooks211 <nash.brooks.2009@gmail.com>
2017-03-04 12:22:22 -0500

    Add it to the thing

--- Downloader/Form1.Designer.vb
+++ Downloader/Form1.Designer.vb
@@ -22,9 +22,46 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Downloadbutton = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(258, 22)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Url"
+        '
+        'Downloadbutton
+        '
+        Me.Downloadbutton.Location = New System.Drawing.Point(81, 40)
+        Me.Downloadbutton.Name = "Downloadbutton"
+        Me.Downloadbutton.Size = New System.Drawing.Size(101, 23)
+        Me.Downloadbutton.TabIndex = 1
+        Me.Downloadbutton.Text = "Download"
+        Me.Downloadbutton.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.Downloadbutton)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Downloadbutton As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
 End Class
--- Downloader/Form1.vb
+++ Downloader/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
-
+    Private Sub Downloadbutton_Click(sender As Object, e As EventArgs) Handles Downloadbutton.Click
+        If TextBox1.Text() = "" Then
+            MessageBox.Show("You have to put a URL in the textbox", "Error")
+        Else
+            SaveFileDialog1.ShowDialog()
+            My.Computer.Network.DownloadFile(TextBox1.Text, SaveFileDialog1.FileName)
+        End If
+    End Sub
 End Class
